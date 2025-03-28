f468583a1e6dfe25a013cce23e565811a8d7e4e0
MagyarL <magyar.levi@gmail.com>
2025-03-28 17:09:55 +0100

    create backend, add required code, create frontend app

---
+++ Task-manager2/TaskManagerApp/Program.cs
@@ -0,0 +1,27 @@
+using TaskManagerApp.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    // beállítjuk, hogy a backend engedélyezze a kéréseket a localhost:4200-ról
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddSignalR();  // Add SignalR
+
+var app = builder.Build();
+
+app.UseCors("CorsPolicy");
+
+app.MapControllers();
+app.MapHub<TaskHub>("/taskHub");  // Hozzárendeljük a "/taskHub" path-hoz a mi TaskHub típusunkat
+
+app.Run();
